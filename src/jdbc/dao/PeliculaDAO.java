e8767ef4e4c339b27d2449cb69e73807538cfe7c
scferrada <scferradaa@gmail.com>
2020-08-03 12:15:09 -0400

    buscador de peliculas

--- src/jdbc/dao/PeliculaDAO.java
+++ src/jdbc/dao/PeliculaDAO.java
@@ -35,4 +35,42 @@ public class PeliculaDAO {
         return peliculas;
     }
 
+    public List<Pelicula> obtenerPeliculasPorActor(String nombreActor) throws SQLException {
+        String sql = "select distinct nombre, año, calificacion " +
+                "from personaje, pelicula " +
+                "where año=p_año and nombre=p_nombre and a_nombre=?";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ps.setString(1, nombreActor);
+        ResultSet rs = ps.executeQuery();
+        List<Pelicula> peliculas = new LinkedList<>();
+        while (rs.next()){
+            Pelicula p = new Pelicula(
+                    rs.getString("nombre"),
+                    rs.getInt("año"),
+                    rs.getDouble("calificacion")
+            );
+            peliculas.add(p);
+        }
+        return peliculas;
+    }
+
+    public List<Pelicula> obtenerPeliculasPorPersonaje(String personaje) throws SQLException {
+        String sql = "select distinct nombre, año, calificacion " +
+                "from personaje, pelicula " +
+                "where año=p_año and nombre=p_nombre and personaje=?";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ps.setString(1, personaje);
+        ResultSet rs = ps.executeQuery();
+        List<Pelicula> peliculas = new LinkedList<>();
+        while (rs.next()){
+            Pelicula p = new Pelicula(
+                    rs.getString("nombre"),
+                    rs.getInt("año"),
+                    rs.getDouble("calificacion")
+            );
+            peliculas.add(p);
+        }
+        return peliculas;
+    }
+
 }
