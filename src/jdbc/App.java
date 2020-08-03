facd0c2225a4302ac74fea56e62fe38521d82d7b
scferrada <scferradaa@gmail.com>
2020-08-03 10:01:09 -0400

    dto/daos

---
+++ src/jdbc/App.java
@@ -0,0 +1,18 @@
+package jdbc;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingresa el nombre de un actor");
+        String nombre = sc.nextLine();
+        ActorDAO dao = new ActorDAO();
+        List<Actor> actores = dao.obtenerActorPorNombreLike("%"+nombre+"%");
+        for (Actor a : actores) {
+            System.out.println(a.getNombre()+", "+a.getGenero());
+        }
+    }
+}
--- src/jdbc/ConnectionTest.java
+++ src/jdbc/ConnectionTest.java
@@ -1,8 +1,7 @@
 package jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Scanner;
 
 public class ConnectionTest {
     public static void main(String[] args) throws SQLException {
@@ -10,5 +9,18 @@ public class ConnectionTest {
                 "jdbc:sqlserver://localhost:1433; databasename=cine; " +
                         "integratedSecurity=true"
         );
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingrese una pelicula");
+        String pelicula = sc.nextLine();
+        String consulta = "SELECT TOP 3 * FROM pelicula " +
+                "WHERE nombre Like ? ORDER BY calificacion DESC";
+        PreparedStatement ps = conn.prepareStatement(consulta);
+        ps.setString(1, pelicula);
+        ResultSet rs = ps.executeQuery();
+        while(rs.next()){
+            System.out.print(rs.getString("nombre")+": ");
+            System.out.println(rs.getDouble("calificacion"));
+        }
     }
 }
