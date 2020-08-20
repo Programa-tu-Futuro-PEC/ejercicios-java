b3f471bc7e42d04dd0c68f04ae0e5d0ebff4b479
scferrada <scferradaa@gmail.com>
2020-08-20 13:35:20 -0400

    ejercicio rut

--- angular/personal/src/app/app.component.ts
+++ angular/personal/src/app/app.component.ts
@@ -13,4 +13,27 @@ export class AppComponent {
     {id: 2, nombre: 'gatito'}
   ]
   columnasAMostrar = ['nombre']
+  usuarios = [
+    {rut:'1-9', nombres: "perico", apellido: "perez", correo: "a@c.l"},
+    {rut:'1-9', nombres: "perico", apellido: "perez", correo: "a@c.l"},
+    {rut:'1-9', nombres: "perico", apellido: "perez", correo: "a@c.l"},
+  ]
+  columnasAMostrar2 = ['rut', 'nombres', 'apellido', 'correo']
+
+  toggleRut(){
+    if(this.columnasAMostrar2.includes('rut')){
+      this.columnasAMostrar2.splice(0,1);
+    } else {
+      this.columnasAMostrar2.unshift('rut');
+    }
+  }
+
+  buttonLabel(){
+    if(this.columnasAMostrar2.includes('rut')){
+      return "Quitar RUT"
+    } else {
+      return "Mostrar RUT"
+    }
+  }
+
 }
